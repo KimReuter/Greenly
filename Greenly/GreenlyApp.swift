75475457d539063adc17d8f8bcdd12a06c4946bd
Kim Reuter <kimbaerlyyo@web.de>
2025-02-09 19:03:57 +0100

    Initial Commit

---
+++ Greenly/GreenlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenlyApp.swift
+//  Greenly
+//
+//  Created by Kim Reuter on 09.02.25.
+//
+
+import SwiftUI
+
+@main
+struct GreenlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
